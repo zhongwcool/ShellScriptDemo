59554d081f4000e70ecd076a282a6ef507734b63
zhongwcool <zhongwcool@gmail.com>
2014-09-24 09:41:48 +0800

    增加重签名功能:resign脚本参数的支持
    当工程被执行new或remake之后可能需要重新签名，执行打包命令时加入resign参数，会直接完成重签名与打包的工作。
    eg. $sh cn_Imagecp.sh resign

--- cn/cn_Imagecp.sh
+++ cn/cn_Imagecp.sh
@@ -1,10 +1,41 @@
 #!/bin/dash
 #author alex.zhong
-#date 2014-07-31
-#version 1.0.3
+#date 2014-09-09
+#version 1.0.5
+################################################################
+# Change log
+# 1.0.5 
+#       增加重签名功能:resign脚本参数的支持
+# 1.0.4 
+#       修改了最后压缩包输出的文件名格式
+# 1.0.3 
+#       精简ImageBin的打包内容
+################################################################
 
+# define feature option
 PROJECT_DAFAULT=lo1
 PROJECT_PATH=$(cd "$(dirname "$0")"; pwd)
+
+RESIGN_ENABLE=false
+QUICK_IMAGE_ENABLE=false
+
+# add a shell script parameter feature support
+if [ $# -ge 1 ]; then
+  for op in $*
+  do
+    case ${op} in
+      "resign") 
+	#echo "we will resign the project image."
+	RESIGN_ENABLE=true;;
+      "quick") 
+	#echo "compress image in quick mode."
+	QUICK_IMAGE_ENABLE=true;;
+      *) 
+	echo "${op} is not valid parameter!"
+	exit 1
+    esac
+  done
+fi
 
 echo -n "1、请输入项目名[eg:${PROJECT_DAFAULT}]:"
 read PROJECT
@@ -23,15 +54,23 @@ if [ ! -d "./out/target/product/${PROJECT}" ]; then
   exit 2
 fi
 
-if [ ! -d "./out/target/product/${PROJECT}/signed_bin/" ]; then
-  echo -n ">>>Image未签名，是否现在签名 Y or N:"
-  read SIGN
-  if [ ${SIGN}=Y -o ${SIGN}=y ]; then
-    echo ">>>签名将花费2～3分钟，请耐心等待"
-    ./mk ${PROJECT} sign-image
-    echo ">>>签名已完成"
-  else
-    exit 3
+# it will re-sign the system image directly if RESIGN_ENABLE==false
+if [ ${RESIGN_ENABLE} = "true" ]; then
+  echo ">>>签名将花费2～3分钟，请耐心等待"
+  ./mk ${PROJECT} sign-image
+  echo ">>>签名已完成"
+else
+  # original sign procedure
+  if [ ! -d "./out/target/product/${PROJECT}/signed_bin/" ]; then
+    echo -n ">>>Image未签名，是否现在签名 Y or N:"
+    read SIGN
+    if [ ${SIGN}=Y -o ${SIGN}=y ]; then
+      echo ">>>签名将花费2～3分钟，请耐心等待"
+      ./mk ${PROJECT} sign-image
+      echo ">>>签名已完成"
+    else
+      exit 3
+    fi
   fi
 fi
 
@@ -49,42 +88,38 @@ echo ">>>文件已删除"
 echo ""
 
 echo "3、Image拷贝"
-mkdir -p "$IMAGESET"
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/boot-sign.img"
-cp -L out/target/product/$PROJECT/signed_bin/boot-sign.img $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/cache-sign.img"
-cp -L out/target/product/$PROJECT/signed_bin/cache-sign.img $IMAGESET
-#cp out/target/product/$PROJECT/DSP_BL $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/EBR1-sign"
-cp -L out/target/product/$PROJECT/signed_bin/EBR1-sign $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/lk-sign.bin"
-cp -L out/target/product/$PROJECT/signed_bin/lk-sign.bin $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/logo-sign.bin"
-cp -L out/target/product/$PROJECT/signed_bin/logo-sign.bin $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/MBR-sign"
-cp -L out/target/product/$PROJECT/signed_bin/MBR-sign $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/MT6572_Android_scatter.txt"
-cp -L out/target/product/$PROJECT/MT6572_Android_scatter.txt $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/preloader_$PROJECT.bin"
-cp -L out/target/product/$PROJECT/preloader_$PROJECT.bin $IMAGESET
-#cp -L out/target/product/$PROJECT/uboot_$PROJECT.bin $IMAGESET
-#cp -L out/target/product/$PROJECT/ramdisk.img $IMAGESET
-#cp -L out/target/product/$PROJECT/ramdisk-recovery.img $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/recovery-sign.img"
-cp -L out/target/product/$PROJECT/signed_bin/recovery-sign.img $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/secro-sign.img"
-cp -L out/target/product/$PROJECT/signed_bin/secro-sign.img $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/system-sign.img"
-cp -L out/target/product/$PROJECT/signed_bin/system-sign.img $IMAGESET
-echo ">>>开始拷贝 out/target/product/$PROJECT/signed_bin/userdata-sign.img"
-cp -L out/target/product/$PROJECT/signed_bin/userdata-sign.img $IMAGESET
+mkdir -p "${IMAGESET}"
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/boot-sign.img"
+cp -L out/target/product/${PROJECT}/signed_bin/boot-sign.img ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/cache-sign.img"
+cp -L out/target/product/${PROJECT}/signed_bin/cache-sign.img ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/EBR1-sign"
+cp -L out/target/product/${PROJECT}/signed_bin/EBR1-sign ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/lk-sign.bin"
+cp -L out/target/product/${PROJECT}/signed_bin/lk-sign.bin ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/logo-sign.bin"
+cp -L out/target/product/${PROJECT}/signed_bin/logo-sign.bin ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/MBR-sign"
+cp -L out/target/product/${PROJECT}/signed_bin/MBR-sign ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/MT6572_Android_scatter.txt"
+cp -L out/target/product/${PROJECT}/MT6572_Android_scatter.txt ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/preloader_${PROJECT}.bin"
+cp -L out/target/product/${PROJECT}/preloader_${PROJECT}.bin ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/recovery-sign.img"
+cp -L out/target/product/${PROJECT}/signed_bin/recovery-sign.img ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/secro-sign.img"
+cp -L out/target/product/${PROJECT}/signed_bin/secro-sign.img ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/system-sign.img"
+cp -L out/target/product/${PROJECT}/signed_bin/system-sign.img ${IMAGESET}
+echo ">>>开始拷贝 out/target/product/${PROJECT}/signed_bin/userdata-sign.img"
+cp -L out/target/product/${PROJECT}/signed_bin/userdata-sign.img ${IMAGESET}
 echo ">>>文件拷贝完成"
 echo ""
 
 echo "4、文件压缩"
 echo ">>>开始压缩.."
-TARDATE=`date +%Y%m%d%H%M`
-cd $IMAGESET
+TARDATE=`date +%Y%m%d-%H%M`
+cd ${IMAGESET}
 TARFILE=${IMAGETAR_PREFIX}${TARDATE}.tar.gz
 tar czvf ${PROJECT_PATH}/${TARFILE} *
 #cd ${PROJECT_PATH}
